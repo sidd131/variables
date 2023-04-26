4d613f1dd8bc5d2073794eb339b3d6bc10927e97
sidd131 <124857670+sidd131@users.noreply.github.com>
2023-04-26 13:38:34 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>       //every c program will start with this . we have included a header file for standard input output .
+int main ()
+{
+int var = 3;
+    float f = 8.5;
+    char c = 'g';
+printf ("%d\n %f\n %c\n", var, f, c);  //%d is the identifier for integers
+return 0;
+}
+//compilation checks for any error in the program . before running , compile it .
+//every project will have only one "main" function
